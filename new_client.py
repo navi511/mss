609e5c913683365af90ffc0e2527dc53a6f3624b
navi511 <55872576+navi511@users.noreply.github.com>
2020-06-02 00:15:13 +0530

    Update new_client.py

--- new_client.py
+++ new_client.py
@@ -3,61 +3,61 @@ from msslogger import MSSLogger
 
 MSSLogger.intializelogger()
 logger = MSSLogger.getlogger("clientlogger")
-s = socket.socket()
+cs = socket.socket()
 host = "127.0.0.1"
 port = 9999
 
-s.connect((host, port))
+cs.connect((host, port))
 
 
 def f_input():
-    choice_msg = s.recv(1024)
-    logger.info("======== Received choice message from server: =========\n "+ choice_msg.decode())
-    print("======== Received choice message from server: =========\n ", choice_msg.decode())
+    choice_msg = cs.recv(1024)
+    logger.info("********* Services Provided by Server: *********\n " + choice_msg.decode())
+    print("********* Services Provided by Server: *********\n ", choice_msg.decode('utf-8'))
     # User will provide the input as he wants to use the service
-    choice_in = input("Please choose the functionality you want to proceed with: ")
-    logger.info("Please choose the functionality you want to proceed with: "+choice_in)
-    s.send(choice_in.encode())
+    choice_in = input("Please choose the Service option: ")
+    logger.info("Please choose the Service option: " + choice_in)
+    cs.send(choice_in.encode('utf-8'))
 
-    choice_rec = s.recv(1024)       # According to selected choice, look for the service
-    logger.info(choice_rec.decode())
-    
-    if choice_rec.decode() == '1':
-        echo_rcv = s.recv(1024).decode()
+    choice_rec = cs.recv(1024)  # According to selected choice, look for the service
+    logger.info(choice_rec.decode('utf-8'))
+
+    if choice_rec.decode('utf-8') == '1':
+        echo_rcv = cs.recv(1024).decode('utf-8')
         print(echo_rcv)
         logger.info(echo_rcv)
         client_echo()
 
-    elif choice_rec.decode() == '2':
+    elif choice_rec.decode('utf-8') == '2':
         file_transfer()
     else:
-        invalidoption()
+        invalid_service()
 
 
-def invalidoption():
-    print("Oops !!! you have entered the wrong input\nDisconnecting from server \a...")
-    logger.error("Oops !!! you have entered the wrong input\nDisconnecting from server \a...")
+def invalid_service():
+    print("Oops !!! you have selected wrong service option\nDisconnecting from server \a...")
+    logger.error("Oops !!! you have selected wrong service option\nDisconnecting from server \a...")
 
 
 def client_echo():
     while True:
-        msg = input("Enter the string: ")
-        logger.info("Enter the string: "+msg)
+        msg = input("Type your Message: ")
+        logger.info("Enter the string: " + msg)
         # send msg to server
-        s.sendall(msg.encode())
+        cs.sendall(msg.encode('utf-8'))
 
         # echo reply from server
-        data = s.recv(1024)
-        print("Received echo message from server: ", data.decode())
-        logger.info("Received echo message from server: "+data.decode())
-        if data.decode() == "Disconnecting from server ...\a":
+        data = cs.recv(1024)
+        print("Response from Server: ", data.decode('utf-8'))
+        logger.info("Received echo message from server: " + data.decode('utf-8'))
+        if data.decode('utf-8') == "Disconnecting from server ...\a":
             break
-    s.close()
+    cs.close()
 
 
 def file_transfer():
     print("Currently the code is under development ")
-    pass                    # here FTS code will come
+    pass  # here FTS code will come
 
 
 def main():
